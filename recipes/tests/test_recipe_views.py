246380c93320e7fbe8f0745f1c6dcbc1d28dc8fb
LuisOtavioSantos <luis.santos@engenharia.ufjf.br>
2022-09-17 08:27:48 -0300

    pytest files

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.models import User
 from django.test import TestCase  # noqa F401
 from django.urls import resolve, reverse
 from recipes import views
-from recipes.models import Category, Recipe
+
+from .test_recipe_base import Recipe, RecipeTestBase
 
 
-class RecipeViewsTest(TestCase):
+class RecipeViewsTest(RecipeTestBase):
+    def tearDown(self) -> None:
+        return super().tearDown()
+
     def test_recipe_home_views_function_is_correct(self):
         # view = resolve('/')
         view = resolve(reverse(viewname='recipes:home'))
@@ -20,43 +23,20 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_template_shows_no_recipe_if_no_recipes(self):
+        # Recipe.objects.get(pk=1).delete()
         response = self.client.get(reverse(viewname='recipes:home'))  # noqa F841
         self.assertIn('No recipes found', response.content.decode('utf-8'))
 
     def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(  # noqa 481
-            category=category,
-            author=author,
-            title='Recipe Title',
-            description='Recipe Description',
-            slug='recipe-slug',
-            preparation_time=10,
-            preparation_time_unit='Minutos',
-            servings=5,
-            servings_unit='Porções',
-            preparation_steps='Recipe Preparation Steps',
-            preparation_steps_is_html=False,
-            is_published=True,
-        )
-
+        self.make_recipe()
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
         # response.context['recipes'].first().title
         # response.context['recipes'].first().category.name
         # response.context['recipes'].first().id
-
+        # check if one recipe exists
         self.assertIn('Recipe Title', content)
-        self.assertIn('10 Minutos', content)
-        self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)
 
     def test_recipe_category_views_function_is_correct(self):
@@ -66,13 +46,13 @@ class RecipeViewsTest(TestCase):
 
     def test_recipe_category_view_return_status_code_404_if_no_recipes_found(self):  # noqa E501
         response = self.client.get(
-            reverse(viewname='recipes:category', kwargs={'category_id': 1})
+            reverse(viewname='recipes:category', kwargs={'category_id': 1000})
          )  # noqa F841
         self.assertEqual(response.status_code, 404)
 
     def test_recipe_detail_view_return_status_code_404_if_no_recipes_found(self):  # noqa E501
         response = self.client.get(
-            reverse(viewname='recipes:recipe', kwargs={'id': 1})
+            reverse(viewname='recipes:recipe', kwargs={'id': 1000})
          )  # noqa F841
         self.assertEqual(response.status_code, 404)
 

8e027ca6f3b88cb29f4e8fd509c37102294caa94
LuisOtavioSantos <luis.santos@engenharia.ufjf.br>
2022-09-23 09:20:20 -0300

    pytest context manager - exception

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,8 +1,10 @@
+from unittest import skip
+
 from django.test import TestCase  # noqa F401
 from django.urls import resolve, reverse
 from recipes import views
 
-from .test_recipe_base import Recipe, RecipeTestBase
+from .test_recipe_base import RecipeTestBase
 
 
 class RecipeViewsTest(RecipeTestBase):
@@ -22,10 +24,12 @@ class RecipeViewsTest(RecipeTestBase):
         response = self.client.get(reverse(viewname='recipes:home'))  # noqa F841
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
+    @skip('Skip this test')
     def test_recipe_home_template_shows_no_recipe_if_no_recipes(self):
         # Recipe.objects.get(pk=1).delete()
         response = self.client.get(reverse(viewname='recipes:home'))  # noqa F841
         self.assertIn('No recipes found', response.content.decode('utf-8'))
+        self.fail('Failure')
 
     def test_recipe_home_template_loads_recipes(self):
         self.make_recipe()
@@ -39,6 +43,12 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIn('Recipe Title', content)
         self.assertEqual(len(response_context_recipes), 1)
 
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """ Test if recipes not published will not show """
+        self.make_recipe(is_published=False)
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn('No recipes found', response.content.decode('utf-8'))
+
     def test_recipe_category_views_function_is_correct(self):
         view = resolve(
             reverse(viewname='recipes:category', kwargs={'category_id': 1}))
@@ -50,6 +60,23 @@ class RecipeViewsTest(RecipeTestBase):
          )  # noqa F841
         self.assertEqual(response.status_code, 404)
 
+    def test_recipe_category_template_loads_recipes(self):
+        self.make_recipe(title='Test_Category')
+
+        response = self.client.get(reverse('recipes:category', args=(1,)))
+        content = response.content.decode('utf-8')
+
+        self.assertIn('Test_Category', content)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """ Test if recipes not published will not show """
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse(viewname='recipes:category', kwargs={
+                    'category_id': recipe.category.id})
+        )
+        self.assertEqual(response.status_code, 404)
+
     def test_recipe_detail_view_return_status_code_404_if_no_recipes_found(self):  # noqa E501
         response = self.client.get(
             reverse(viewname='recipes:recipe', kwargs={'id': 1000})
@@ -59,3 +86,20 @@ class RecipeViewsTest(RecipeTestBase):
     def test_recipe_detail_views_function_is_correct(self):
         view = resolve(reverse(viewname='recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_template_loads_the_correct_recipes(self):
+        self.make_recipe(title='Test_Category')
+
+        response = self.client.get(reverse('recipes:recipe', kwargs={'id': 1}))
+        content = response.content.decode('utf-8')
+
+        self.assertIn('Test_Category', content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        """ Test if recipes not published will not show """
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(
+            reverse(viewname='recipes:recipe', kwargs={
+                    'id': recipe.id})
+        )
+        self.assertEqual(response.status_code, 404)

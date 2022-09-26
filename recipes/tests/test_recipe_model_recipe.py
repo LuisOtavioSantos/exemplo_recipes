4595fd0a70e733863d5286e850b80ced82654641
LuisOtavioSantos <luis.santos@engenharia.ufjf.br>
2022-09-26 15:37:46 -0300

    test parameterized - coverage init

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,13 +1,34 @@
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import Recipe, RecipeTestBase
 
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
+
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Default Category'),
+            author=self.make_author(username='newuser'),
+            title='Recipe Title',
+            description='Recipe Description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            # os dois ultimos campos não podem ser enviados,
+            # pois queremos testá-los
+            # preparation_steps_is_html=False,
+            # is_published=True,
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
 
     def test_recipe_title_raises_error_if_more_65(self):
         self.recipe.title = "A"*70
@@ -26,6 +47,16 @@ class RecipeModelTest(RecipeTestBase):
         """ Este teste em específico não é reconhecido pelo unittest
         pois ele possui subtestes, neste caso deve-se utilizar no terminal
         o seguinte comando python manage.py test"""
-        setattr(self.recipe, field, 'A'*(max_length + 0))
+        setattr(self.recipe, field, 'A'*(max_length + 1))
         with self.assertRaises(ValidationError):  # context manager
             self.recipe.full_clean()  # Aqui a validação ocorre
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(recipe.preparation_steps_is_html,
+                         msg='Recipe preparation_steps_is_html is not False')
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(recipe.is_published,
+                         msg='Recipe is_published is not False')

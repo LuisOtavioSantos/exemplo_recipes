05cf3b8e4ea3e96bad984271e2e82aef252336e6
LuisOtavioSantos <luis.santos@engenharia.ufjf.br>
2022-10-07 13:32:11 -0300

    form to register users

--- authors/views.py
+++ authors/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
+from .forms import FormRegister
+
 # Create your views here.
 
 
 def register_author(request):
-    return render(request=request, template_name='authors/pages/register_view.html')
+    form = FormRegister()
+    return render(request=request,
+                  template_name='authors/pages/register_view.html',
+                  context={'form': form})

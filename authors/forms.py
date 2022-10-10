c6e1173ec5d76e3447594edbe30587405251db9f
LuisOtavioSantos <luis.santos@engenharia.ufjf.br>
2022-10-10 13:35:25 -0300

    modifying forms

--- authors/forms.py
+++ authors/forms.py
@@ -14,3 +14,32 @@ class FormRegister(forms.ModelForm):
             'email',
             'password',
         ]
+        # ou
+        # exclude = ['first_name']
+        labels = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'username': 'Type a Username',
+            'email': 'best email',
+            'password': 'Create a password',
+        }
+        help_texts = {
+            'email': 'The e-mail must be valid',
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type First Name Here'
+            }),
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Type Last Name Here'
+            }),
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type Username Here'
+            }),
+            'email': forms.TextInput(attrs={
+                'placeholder': 'Type e-mail Here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type Password Here'
+            }),
+        }

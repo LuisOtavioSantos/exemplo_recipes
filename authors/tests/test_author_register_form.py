7258917526dc2e059895dda94c9ad09d4b807b83
LuisOtavioSantos <luis.santos@engenharia.ufjf.br>
2022-10-11 00:28:28 -0300

    testing help text

--- authors/tests/test_author_register_form.py
+++ authors/tests/test_author_register_form.py
@@ -13,7 +13,17 @@ class AuthorRegisterFormUnitTest(TestCase):
         ('password', 'Write password Here !!!'),
         ('password2', 'Repeat Your Password !!!'),
     ])
-    def test_first_name_placeholder_is_correct(self, field, message):
+    def test_fields_placeholder_is_correct(self, field, message):
         form = FormRegister()
         placeholder = form[field].field.widget.attrs['placeholder']
         self.assertEqual(message, placeholder)
+
+    @parameterized.expand([
+        ('email', 'The e-mail must be valid'),
+        ('password', 'Password must have one lower case letter and one number'),
+        ('password2', 'Password must have one lower case letter and one number'),
+    ])
+    def test_fields_help_text(self, field, message):
+        form = FormRegister()
+        current = form[field].field.help_text
+        self.assertEqual(message, current)

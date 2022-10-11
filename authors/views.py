8b17dcd5f89cf2bb889410a57bed58339f7c3a67
LuisOtavioSantos <luis.santos@engenharia.ufjf.br>
2022-10-10 23:39:31 -0300

    forms - user created - session info deleted

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -21,5 +22,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = FormRegister(POST)  # noqa F841
-
+    if form.is_valid():
+        form.save()
+        # data = form.save(commit=False)
+        # data.outro_campo = 'outro valor'
+        messages.success(
+            request=request, message='User created! \nLogin Available.')
+        del (request.session['register_form_data'])
     return redirect(to='authors:register')
